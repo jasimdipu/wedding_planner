48b3b3dcab4ad48ce436cfd62441d1dfca2520f9
jasimdipu <dipu.j247@gmail.com>
2021-12-06 20:21:32 +0600

    Models updated

--- locations/models.py
+++ locations/models.py
@@ -2,11 +2,24 @@ from django.db import models
 
 # Create your models here.
 
-class City(models.Model):
-    pass
 
 class Country(models.Model):
-    pass
+    country_name = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.country_name
+
+
+class City(models.Model):
+    city_name = models.CharField(max_length=255, blank=True, null=True)
+    postal_code = models.CharField(max_length=255, blank=True, null=True)
+    country_id = models.ForeignKey(Country, null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self) -> str:
+        return self.city_name
+
 
 class Location(models.Model):
-    pass
+    location_name = models.CharField(max_length=255, blank=True, null=True)
+    city_id = models.ForeignKey(
+        City, null=True, blank=True, on_delete=models.SET_NULL)
