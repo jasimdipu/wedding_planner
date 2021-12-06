2e1cc65bcc6c4e86618f6b0350f94e007dd7c3dd
jasimdipu <dipu.j247@gmail.com>
2021-12-06 20:41:17 +0600

    Models updated

--- participants/models.py
+++ participants/models.py
@@ -1,15 +1,37 @@
 from django.db import models
-
+from weddings.models import Wedding, Event
 # Create your models here.
 
+
 class InEvent(models.Model):
-    pass
+    event_id = models.ForeignKey(
+        Event, null=True, blank=True, on_delete=models.SET_NULL)
+    participate_id = models.ForeignKey(
+        "Participate", null=True, blank=True, on_delete=models.SET_NULL)
+    details = models.TextField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.event_id
+
 
 class Participate(models.Model):
-    pass
+    wedding_id = models.ForeignKey(
+        Wedding, null=True, blank=True, on_delete=models.SET_NULL)
+    person_id = models.ForeignKey(
+        "Person", null=True, blank=True, on_delete=models.SET_NULL)
+
 
 class Person(models.Model):
-    pass
+    person_code = models.CharField(max_length=20, null=True, blank=True)
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.first_name
+
 
 class Role(models.Model):
-    pass
+    role_name = models.CharField(max_length=64, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.role_name
--- partner_product_services/models.py
+++ partner_product_services/models.py
@@ -2,20 +2,60 @@ from django.db import models
 
 # Create your models here.
 
+
 class Partner(models.Model):
-    pass
+    partner_code = models.CharField(max_length=20, null=True, blank=True)
+    partner_name = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.partner_name
+
 
 class Product(models.Model):
-    pass
+    product_code = models.CharField(max_length=20, null=True, blank=True)
+    product_name = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(
+        upload_to="static/service_image", null=True, blank=True)
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.product_name
+
 
 class ProvideProduct(models.Model):
-    pass
+    pertner_id = models.ForeignKey(
+        Partner, null=True, blank=True, on_delete=models.SET_NULL)
+    product_id = models.ForeignKey(
+        Product, null=True, blank=True, on_delete=models.SET_NULL)
+    description = models.TextField(null=True, blank=True)
+    is_active = models.BooleanField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.pertner_id
+
 
 class ProvidesService(models.Model):
-    pass
+    pertner_id = models.ForeignKey(
+        Partner, null=True, blank=True, on_delete=models.SET_NULL)
+    service_id = models.ForeignKey(
+        "Service", null=True, blank=True, on_delete=models.SET_NULL)
+    description = models.TextField(null=True, blank=True)
+    is_active = models.BooleanField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.pertner_id
+
 
 class Service(models.Model):
-    pass
+    service_code = models.CharField(max_length=20, null=True, blank=True)
+    service_name = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(
+        upload_to="static/service_image", null=True, blank=True)
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True, blank=True)
 
-
-
+    def __str__(self) -> str:
+        return self.service_name
