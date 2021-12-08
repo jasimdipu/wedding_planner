fba39e03f553796186b9982b49a3b3f9f0bf2bc6
jasimdipu <dipu.j247@gmail.com>
2021-12-08 11:21:16 +0600

    models updated

--- weddings/models.py
+++ weddings/models.py
@@ -7,7 +7,8 @@ from partner_product_services.models import Partner, Product, Service
 
 class Event(models.Model):
     event_name = models.CharField(max_length=200, null=True, blank=True)
-    wedding_id = models.ForeignKey("Wedding", null=True, blank=True, on_delete=models.SET_NULL)
+    wedding_id = models.ForeignKey(
+        "Wedding", null=True, blank=True, on_delete=models.SET_NULL)
     location_id = models.ForeignKey(
         Location, null=True, blank=True, on_delete=models.SET_NULL)
     start_time_planned = models.DateTimeField(null=True, blank=True)
@@ -22,47 +23,57 @@ class Event(models.Model):
 
 
 class ProductIncluded(models.Model):
-    event_id = models.ForeignKey(Event, null=True, blank=True, on_delete=models.SET_NULL)
-    provides_product_id = models.ForeignKey(
-        "ProvidesProduct", null=True, blank=True, on_delete=models.SET_NULL)
-    price = models.DecimalField(
-        max_digits=10, decimal_places=2, null=True, blank=True)
-    current_status_id = models.ForeignKey("Status", null=True, blank=True, on_delete=models.SET_NULL)
+    # event_id = models.ForeignKey(
+    #     Event, null=True, blank=True, on_delete=models.SET_NULL)
+    # provides_product_id = models.ForeignKey(
+    #     "ProvidesProduct", null=True, blank=True, on_delete=models.SET_NULL)
+    # price = models.DecimalField(
+    #     max_digits=10, decimal_places=2, null=True, blank=True)
+    # current_status_id = models.ForeignKey(
+    #     "Status", null=True, blank=True, on_delete=models.SET_NULL)
 
-    def __str__(self) -> str:
-        return self.event_id
+    # def __str__(self) -> str:
+    #     return self.event_id
+    pass
+
 
 class ProvidesProduct(models.Model):
-    partner_id = models.ForeignKey(Partner, null=True, blank=True, on_delete=models.SET_NULL)
-    product_id = models.ForeignKey(Product, null=True, blank=True, on_delete=models.SET_NULL)
-    details = models.TextField(null=True, blank=True)
+    # partner_id = models.ForeignKey(
+    #     Partner, null=True, blank=True, on_delete=models.SET_NULL)
+    # product_id = models.ForeignKey(
+    #     Product, null=True, blank=True, on_delete=models.SET_NULL)
+    # details = models.TextField(null=True, blank=True)
 
-    def __str__(self) -> str:
-        return self.partner_id
+    # def __str__(self) -> str:
+    #     return self.partner_id
+    pass
 
 
 class ProvidesService(models.Model):
-    partner_id = models.ForeignKey(
-        Partner, null=True, blank=True, on_delete=models.SET_NULL)
-    service_id = models.ForeignKey(
-        Service, null=True, blank=True, on_delete=models.SET_NULL)
-    details = models.TextField(null=True, blank=True)
+    # partner_id = models.ForeignKey(
+    #     Partner, null=True, blank=True, on_delete=models.SET_NULL)
+    # service_id = models.ForeignKey(
+    #     Service, null=True, blank=True, on_delete=models.SET_NULL)
+    # details = models.TextField(null=True, blank=True)
 
-    def __str__(self) -> str:
-        return self.partner_id
+    # def __str__(self) -> str:
+    #     return self.partner_id
+    pass
+
 
 class ServiceIncluded(models.Model):
-    event_id = models.ForeignKey(
-        Event, null=True, blank=True, on_delete=models.SET_NULL)
-    provides_service_id = models.ForeignKey(
-        ProvidesService, null=True, blank=True, on_delete=models.SET_NULL)
-    price = models.DecimalField(
-        max_digits=10, decimal_places=2, null=True, blank=True)
-    current_status_id = models.ForeignKey(
-        "Status", null=True, blank=True, on_delete=models.SET_NULL)
+    # event_id = models.ForeignKey(
+    #     Event, null=True, blank=True, on_delete=models.SET_NULL)
+    # provides_service_id = models.ForeignKey(
+    #     ProvidesService, null=True, blank=True, on_delete=models.SET_NULL)
+    # price = models.DecimalField(
+    #     max_digits=10, decimal_places=2, null=True, blank=True)
+    # current_status_id = models.ForeignKey(
+    #     "Status", null=True, blank=True, on_delete=models.SET_NULL)
 
-    def __str__(self) -> str:
-        return self.event_id
+    # def __str__(self) -> str:
+    #     return self.event_id
+    pass
 
 class Status(models.Model):
     status_name = models.CharField(max_length=200, null=True, blank=True)
@@ -74,15 +85,15 @@ class Status(models.Model):
         return self.status_name
 
 
-
 class Wedding(models.Model):
-    wedding_code = models.CharField(max_length=200, null=True, blank=True)
-    start_time_planned = models.DateTimeField(null=True, blank=True)
-    end_time_planned = models.DateTimeField(null=True, blank=True)
-    start_time = models.DateTimeField(null=True, blank=True)
-    end_time = models.DateTimeField(null=True, blank=True)
-    budget_planned = models.DecimalField(
-        max_digits=7, decimal_places=2, null=True, blank=True)
+    # wedding_code = models.CharField(max_length=200, null=True, blank=True)
+    # start_time_planned = models.DateTimeField(null=True, blank=True)
+    # end_time_planned = models.DateTimeField(null=True, blank=True)
+    # start_time = models.DateTimeField(null=True, blank=True)
+    # end_time = models.DateTimeField(null=True, blank=True)
+    # budget_planned = models.DecimalField(
+    #     max_digits=7, decimal_places=2, null=True, blank=True)
 
-    def __str__(self) -> str:
-        return self.wedding_code
+    # def __str__(self) -> str:
+    #     return self.wedding_code
+    pass
